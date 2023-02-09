4f8a4f98bb0dbe2367e4d32bc736e8e2857bc0cf
Troy Hofstrand <troy.hofstrand@slu.edu>
2023-02-08 21:18:16 -0600

    Created Vue files and basic site

---
+++ vue-proj/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{ title }} </h1>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data () {
+    return{
+      title: 'Hello World'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px
+}
+</style>
