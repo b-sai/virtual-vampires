43ec7fe30e94ad658b8fbbb0b77e37d604836016
codingwithblackcoffee <64619611+codingwithblackcoffee@users.noreply.github.com>
2023-03-14 02:27:28 -0500

    merging user login branch files to dev branch

---
+++ duolango-frontend/src/firebase/index.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwGC1fvk2vmtNIZ-wyzS8WDPx5g9xA83s",
+  authDomain: "worse-duolingo.firebaseapp.com",
+  databaseURL: "https://worse-duolingo-default-rtdb.firebaseio.com",
+  projectId: "worse-duolingo",
+  storageBucket: "worse-duolingo.appspot.com",
+  messagingSenderId: "562797851309",
+  appId: "1:562797851309:web:d9d34341f2cd6e4f400c24"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+export { auth }
