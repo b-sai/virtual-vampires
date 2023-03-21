7bf0de5b4bc0bee6e28a7d578f1e6e100e170ffb
thofstrand <keerplm@gmail.com>
2023-03-20 21:40:55 -0500

    removed const in firebase file THIS TIME

--- duolango-frontend/src/firebase/index.js
+++ duolango-frontend/src/firebase/index.js
@@ -7,15 +7,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // add const firebaseConfig from firebase project
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCwGC1fvk2vmtNIZ-wyzS8WDPx5g9xA83s",
-    authDomain: "worse-duolingo.firebaseapp.com",
-    databaseURL: "https://worse-duolingo-default-rtdb.firebaseio.com",
-    projectId: "worse-duolingo",
-    storageBucket: "worse-duolingo.appspot.com",
-    messagingSenderId: "562797851309",
-    appId: "1:562797851309:web:d9d34341f2cd6e4f400c24"
-  };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
