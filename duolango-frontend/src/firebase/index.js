6d4e3dc2c72b0d3d3ad343d7834836423c88859a
thofstrand <keerplm@gmail.com>
2023-03-30 13:13:01 -0500

    fixed firebase key issue, change .env

--- duolango-frontend/src/firebase/index.js
+++ duolango-frontend/src/firebase/index.js
@@ -8,6 +8,17 @@ import { getAuth } from "firebase/auth";
 // add const firebaseConfig from firebase project
 
 
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_apiKey,
+    authDomain: process.env.VUE_APP_authDomain ,
+    databaseURL:process.env.VUE_APP_databaseURL ,
+    projectId: process.env.VUE_APP_projectId,
+    storageBucket: process.env.VUE_APP_storageBucket,
+    messagingSenderId: process.env.VUE_APP_storageBucket,
+    appId: process.env.VUE_APP_appId
+  };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
