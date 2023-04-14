147bf03cf5e0c6a7124399d2af1e24d607cf56fa
codingwithblackcoffee <64619611+codingwithblackcoffee@users.noreply.github.com>
2023-04-13 23:12:06 -0500

    Complets the logic for all the tense conversions except impertive in swahili language

--- _backend/backend_scripts/converters/SwahiliDictionary.py
+++ _backend/backend_scripts/converters/SwahiliDictionary.py
@@ -1,4 +1,4 @@
 pronouns =  {"1s":"ni", "2s":"u", "3s":"a", "1p":"tu", "2p":"m", "3p":"wa"}
-tense = {"Present": "na", "Future":"ta", "Past":"li"}
-tense_negations = {"Present": "i", "Future":"ta", "Past":"ku"}
+tense = {"Present": "na", "Future":"ta", "Past":"li", "Perfect":"me"}
+tense_negations = {"Present": "i", "Future":"ta", "Past":"ku", "Perfect":"ja"}
 pronouns_negation = {"1s":"si", "2s":"hu", "3s":"ha", "1p":"hatu", "2p":"ham", "3p":"hawa"}
--- _backend/backend_scripts/converters/swahili.py
+++ _backend/backend_scripts/converters/swahili.py
@@ -1,44 +1,76 @@
 from .converter import TenseConverter
-from SwahiliDictionary import pronouns, tense, tense_negations, pronouns_negation
+from .SwahiliDictionary import pronouns, tense, tense_negations, pronouns_negation
 
 class SwahiliConverter(TenseConverter):
 
-    def __init__(self):
+    def __init__(self, irregular_verbs:dict = None):
         super().__init__()
+        # self.irregular_verbs = irregular_verbs
         # add any exception files here
     def to_past(self, verb, is_neg, sp):
+        # if is_neg and verb[2:] == "la":
+        #     return pronouns_negation[sp] + tense_negations["Past"] + verb
         if is_neg:
-            return pronouns_negation[sp] + Negation_tense["Past"] + verb[2:]
+            return pronouns_negation[sp] + tense_negations["Past"] + verb[2:]
+        elif verb[2:] in ["la", "fa", "ja"]:
+            return pronouns[sp]  + tense["Past"] + verb
         else:
-            return pronouns[sp] + tense["Past"] + verb[2:]
+            return pronouns[sp]  + tense["Past"] + verb[2:]
     def to_present(self,verb, is_neg, sp):
         if is_neg:
             if verb[-1] == "a":
                 return (pronouns_negation[sp] + verb[2:-1] + "i")
             else:
                 return (pronouns_negation[sp] +  verb[2:])
+        elif verb[2:] in ["la", "fa", "ja"]:
+            return pronouns[sp]  + tense["Present"] + verb
         else:
             return pronouns[sp]  + tense["Present"] + verb[2:]
     def to_future(self, verb, is_neg, sp):
+        if is_neg and verb[2:] in ["la", "fa", "ja"]:
+            return pronouns_negation[sp] + tense_negations["Future"] + verb
         if is_neg:
-            return pronouns_negation[sp] + Negation_tense["Future"] + verb[2:]
+            return pronouns_negation[sp] + tense_negations["Future"] + verb[2:]
+        elif verb[2:] == "la":
+            return pronouns[sp]  + tense["Future"] + verb
         else:
-            return pronouns[sp] + tense["Future"] + verb[2:]
+            return pronouns[sp]  + tense["Future"] + verb[2:]
 
     def to_past_perfect(self, verb, is_neg, sp):
-        return "Not Done Yet"
+    #confirm the rules with doctor scannel, have some doubts here regarding how singular and plural are trated
+    #in past perfect negation    
+        if is_neg and sp[1] != "s" and verb[2:] in ["la", "fa", "ja"]:
+            return pronouns_negation[sp] + tense_negations["Perfect"] + verb
+        if is_neg:
+            return pronouns_negation[sp] + tense_negations["Perfect"] + verb[2:]
+        elif verb[2:] == "la":
+            return pronouns[sp]  + tense["Perfect"] + verb
+        else:
+            return pronouns[sp]  + tense["Perfect"] + verb[2:]
     
-    def to_imperfect(self, verb, is_neg, sp):
-        return verb[2:]
+    def to_imperitive(self, verb, is_neg, sp):
+        # if(verb not in self.irregular_verbs and sp == "2s"):
+        #     return verb[2:]
+        # elif(verb not in self.irregular_verbs and sp == "2p"):
+        #     if verb[-1] == "a":
+        #         return verb[2:-1] + "eni"
+        #     else:
+        #         return verb[2:] + "ni"
+        # elif(verb in self.irregular_verbs and sp == "2s"):
+        #     #return irregular_verbs[verb]
+        # elif(verb in self.irregular_verbs and sp == "2p"):
+        #     #return irregular_verbs[verb]
+        return ""
 
+    def to_imperfect(self, verb, is_neg, sp):
+        return ""
     def method_dispatcher(self,method_name):
         methods = {"PAST":self.to_past, "PRES":self.to_present, "FUT":self.to_future, 
-                    "PERF":self.to_past_perfect, "IMP":self.to_imperfect}
+                    "PERF":self.to_past_perfect, "IMP":self.to_imperitive}
         return methods[method_name]
     def generate_sentence(self, features):
         feats = features.split("+")
         verb = feats[-1]
-        #####finish this tomorrow####
         if feats[0] == "NEG":
             sp = feats[1]
             tense = feats[2]
