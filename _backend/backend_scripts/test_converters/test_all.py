b631215c4629ab2ca5086b04a05d83fde2d5f915
b-sai <bsaishreyas@gmail.com>
2023-05-01 14:42:29 -0500

    add swahili test to actions

--- _backend/backend_scripts/test_converters/test_all.py
+++ _backend/backend_scripts/test_converters/test_all.py
@@ -5,6 +5,7 @@ from . import test_english
 def suite():
     test_loader = unittest.TestLoader()
     test_names = ['_backend.backend_scripts.test_converters.test_english',
+                  '_backend.backend_scripts.test_converters.test_swahili',
                   '_backend.backend_scripts.test_converters.test_spanish']
 
     test_suite = test_loader.loadTestsFromNames(test_names)
